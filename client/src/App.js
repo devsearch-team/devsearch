72afb1363d82298d6bd24d5f913fad58923fb473
MrAjMann <anthonyjmann87@gmail.com>
2021-07-19 20:24:54 +1000

    AR-10: Added basic Navbar with styled components Global Styles: Set up a globalstyles file, and created a color theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { Styles, theme } from "../src/globalStyles";
 
 const App = () => {
-  return <div>DevSearch</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Styles />
+      Hellow World
+    </ThemeProvider>
+  );
 };
 
 export default App;
