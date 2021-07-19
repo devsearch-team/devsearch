8fbd0d8e410c2c657d63336d2fa19d390c585f6d
robaElshazly <roba1818@gmail.com>
2021-07-19 12:31:31 +1000

    adde initial landing page

---
+++ client/src/pages/landing.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from 'styled-components' 
+
+const Landing = () => {
+  return <div>DevSearch</div>;
+};
+
+
+
+//styles
+const Header = styled.h1 `
+	font-size: 48;
+	color: white;
+`
+const Text=styled.p `
+    font-size:24;
+    color:white;
+`
+const image=styled.img `
+`
+const textinput=styled.Input `
+`
+export default Landing;
