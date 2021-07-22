414067653cce6dd37e426add547ac2aa8b40f824
MrAjMann <anthonyjmann87@gmail.com>
2021-07-22 12:42:12 +1000

    AR-21: Started working on status tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+// import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import NavBar from "./globalComponents/NavBar";
+import Tabs from "./globalComponents/Tabs";
 
 import { Container, Styles, theme } from "../src/globalStyles";
-// import SideBar from "./globalComponents/SideBar";
+import SideBar from "./globalComponents/SideBar";
 import LandingPage from "./pages/landingPage";
 import EmpLogIn from "./pages/EmpLogIn";
-import SeekerLogIn from "./pages/SeekerLogIn"
- 
+import SeekerLogIn from "./pages/SeekerLogIn";
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Styles /> 
-      <BrowserRouter>
-       <NavBar />
-          <Switch>
-            <Route exact path="/" component={LandingPage}/>
-            <Route exact path="/employer/login" component={EmpLogIn}/>
-            {/* <Route exact path="/employer/singup" component={EmpSignUP}/> */}
-            <Route exact path="/seeker/login" component={SeekerLogIn}/>
-            {/* <Route exact path="/seeker/singup" component={SeekerSignUP}/> */}
-          </Switch>
-        </BrowserRouter>
+      <Styles />
+      <NavBar />
+      {/* Past BrowserRouter when finished tabs */}
       {/* <LandingPage />     */}
       {/* The Container and sidebar need to conditionally loaded when a user / employer is logged in */}
-      <Container>{/* <SideBar /> */}</Container>
+      <Container>
+        <SideBar />
+        <Tabs></Tabs>
+      </Container>
     </ThemeProvider>
   );
 };
---
+++ client/src/globalComponents/Tabs.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { theme } from "../globalStyles";
+
+const jobSeekerStatusTabs = [
+  {
+    name: "Applied",
+    id: 1,
+  },
+  {
+    name: "Interview Offered",
+    id: 2,
+  },
+  {
+    name: "Interviewing",
+    id: 3,
+  },
+  {
+    name: "Offers Recieved",
+    id: 4,
+  },
+  {
+    name: "Hired",
+    id: 5,
+  },
+  {
+    name: "Rejected",
+    id: 6,
+  },
+];
+const EmployerStatusTabs = [
+  {
+    name: "Recieved",
+    id: 0,
+  },
+  {
+    name: "Accepted",
+    id: 1,
+  },
+  {
+    name: "Interviewing",
+    id: 2,
+  },
+  {
+    name: "Offer Made",
+    id: 3,
+  },
+  {
+    name: "Hired",
+    id: 4,
+  },
+  {
+    name: "Rejected",
+    id: 5,
+  },
+];
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTab = (e) => {
+    // if (EmployerStatusTabs.id === e.target.id) {
+    //   console.log(EmployerStatusTabs.id);
+    //   setActiveTab(false);
+    // } else {
+    //   console.log(EmployerStatusTabs.id);
+    // }
+    setActiveTab(e.target.id);
+    // console.log(e.target.id);
+  };
+  //   console.log("ActiveTab", activeTab);
+
+  return (
+    <TabsContainer>
+      {EmployerStatusTabs.map((tab, index) => {
+        return (
+          <Tab
+            id={index}
+            activeTab={activeTab}
+            key={index}
+            setSelectedTab={setActiveTab}
+            onClick={handleTab}
+          >
+            {tab.name}
+          </Tab>
+        );
+      })}
+    </TabsContainer>
+  );
+};
+
+export default Tabs;
+
+const TabsContainer = styled.div`
+position:absolute;
+top:25%;
+margin-left: 7rem;
+display: flex;
+width:80%;
+height 100px;
+`;
+
+const Tab = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  margin: 1rem;
+  width: 130px;
+  height: 60px;
+  font-size: 18px;
+
+  background: ${(props) => {
+    console.log(props.id);
+    console.log("active tab", props.activeTab);
+    return props.id === props.activeTab
+      ? theme.PrimaryBtnBg
+      : theme.SecondaryBtnBg;
+  }};
+  color: ${(props) => theme.PrimaryTxt};
+  cursor: pointer;
+  &:hover {
+    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.7);
+  }
+`;
