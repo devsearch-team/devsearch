2fc0bf59ca51e1d832af14076faeab387677bd56
robaElshazly <roba1818@gmail.com>
2021-07-29 17:57:19 +1000

    Added job seeker schema

---
+++ server/models/seeker.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const normalize=require('normalize-mongoose')
+const bcrypt = require('bcrypt')
+
+const Seeker=new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    hash_password:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: String
+    },
+    website:{
+        type: String
+    },
+    about:{
+         type: String
+        },
+    resume:{
+        type: String
+    },
+    facebook:{type:String},
+    instagram:{ type: String},
+    twitter:{ type: String},
+    other:{ type: String}
+})
+
+Employer.methods.comparePassword = function(password){
+    return bcrypt.compareSync(password, this.hash_password)
+}
+module.exports=mongoose.model('Seeker',Seeker)
