74555905d5a3e43acd4ba3549658d73417e2398e
robaElshazly <roba1818@gmail.com>
2021-08-03 11:35:18 +1000

    AR-43: Fixed the seed file, added API with category and search query strings

--- server/utils/jobsUtils.js
+++ server/utils/jobsUtils.js
@@ -3,22 +3,39 @@ const Job = require('../models/job')
 const getAllJobs = function (req) {
     const page = parseInt(req.query.page, 10) || 0
     const limit = parseInt(req.query.limit, 10) || 10
-    const jobs = Job.find()
-            .skip(page * limit)
-            .limit(limit).populate("employer")
+
+    const params = {
+        $text: req.query.search ? { $search: req.query.search } : undefined, // full text search index
+        category: req.query.category,
+        location: req.query.location,
+    }
+    const findArgs = Object.fromEntries(Object.entries(params)
+        .filter(([_, v]) => v !== undefined && v !== '') //remove unused keys
+        .map(([k, v]) => {
+            if(k === '$text') return [k,v] // ignore case insensetive for index search
+            return [k, { $regex: v, $options: 'i' }] // case insensetive 
+        })
+    )
+    console.log(findArgs);
+
+
+    const jobs = Job.find(findArgs)
+        .skip(page * limit)
+        .limit(limit).populate("employer")
+
     return jobs
 }
 
-const getEmployerJobs= function(req){
+const getEmployerJobs = function (req) {
     const page = parseInt(req.query.page, 10) || 0
     const limit = parseInt(req.query.limit, 10) || 10
-    const jobs = Job.find({employer: req.user.id})
-            .skip(page * limit)
-            .limit(limit)
+    const jobs = Job.find({ employer: req.user.id })
+        .skip(page * limit)
+        .limit(limit)
     return jobs
 }
 
-const getJobById = function (id){
+const getJobById = function (id) {
     return Job.findById(id).populate("employer")
 }
 
@@ -33,11 +50,11 @@ const addJob = function (req) {
 
 const updateJob = function (req) {
     req.body.modified_at = Date.now()
-    filter={_id: req.params.id, employer: req.user.id}
+    filter = { _id: req.params.id, employer: req.user.id }
     return Job.findOneAndUpdate(filter, req.body, { new: true })
 }
 
-const checkEmpOwnership=(req, res, next) => {
-    
+const checkEmpOwnership = (req, res, next) => {
+
 }
-module.exports = { getAllJobs, addJob,updateJob,getJobById,getEmployerJobs}
+module.exports = { getAllJobs, addJob, updateJob, getJobById, getEmployerJobs }
